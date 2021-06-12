604ac244f5a170431bc0d1a94804208b08b4d53c
Dizlat <dizlat.ur@gmail.com>
2021-06-12 14:11:15 +0600

    ad

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -16,10 +16,11 @@ class Post(models.Model):
     def __str__(self):
         return '{}'.format(self.title)
 
+
 class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.title
+        return '{}'.format(self.title)
 
--- blogengine/blog/urls.py
+++ blogengine/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', posts_list, name='posts-list'),
     path('post/<str:slug>/', post_detail, name='post-detail'),
     path('tags/', tags_list, name='tags-list'),
+    path('tag/<str:slug>/', tag_detail, name='tag-detail'),
 ]
